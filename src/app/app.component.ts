92e7806716ed6c316551e771c6eab1e214f1d2d1
Hector Fabian Rodriguez Acosta <hector.fabian.rodriguez@outlook.com>
2021-11-23 23:10:47 -0500

    aprendiendo estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  items = ['nicolas', 'julian', 'camilo'];
+
+
+  addItem(): void {
+    this.items.push('nuevo item');
+  }
+  deleteItem(index: number): void {
+    this.items.splice(index, 1);
+  }
 }
